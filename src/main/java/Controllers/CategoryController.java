7dec4cbf477ae27399b10922e7ede07bc811e75e
helenosheaa <helenosheaa@gmail.com>
2018-07-19 17:02:08 +0100

    index page for categories working

--- src/main/java/Controllers/CategoryController.java
+++ src/main/java/Controllers/CategoryController.java
@@ -1,5 +1,18 @@
 package Controllers;
 
+import db.helpers.DBCategory;
+import db.helpers.DBVisitor;
+import models.Category;
+import models.Visitor;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class CategoryController {
 
     public CategoryController (){
@@ -7,6 +20,17 @@ public class CategoryController {
     }
 
     public void setupEndPoints(){
+
+        get("/categories", (req, res) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/categoryTemplates/index.vtl");
+
+            List<Category> categories = DBCategory.getAll();
+            model.put("categories", categories);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
 
     }
 }
