e26d4586841fcd22c184cab3262f0f8a6e6d3513
helenosheaa <helenosheaa@gmail.com>
2018-07-19 16:46:44 +0100

    index page for visitors working

--- src/main/java/Controllers/VisitorController.java
+++ src/main/java/Controllers/VisitorController.java
@@ -1,5 +1,18 @@
 package Controllers;
 
+import db.helpers.DBVisitor;
+import db.helpers.DBWriter;
+import models.Visitor;
+import models.Writer;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class VisitorController {
 
     public VisitorController (){
@@ -7,6 +20,17 @@ public class VisitorController {
     }
 
     public void setupEndPoints(){
+
+        get("/visitors", (req, res) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/visitorTemplates/index.vtl");
+
+            List<Visitor> visitors = DBVisitor.getAll();
+            model.put("visitors", visitors);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
 
     }
 }
