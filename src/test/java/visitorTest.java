09f810a68c71ece677c4d2964d04707d138935d9
StuartCharters <charters.stuart@gmail.com>
2018-07-19 15:27:21 +0100

    passing visitor test

--- src/test/java/visitorTest.java
+++ src/test/java/visitorTest.java
@@ -1,2 +1,20 @@
+import models.Visitor;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class visitorTest {
+
+    private Visitor visitor;
+
+    @Before
+    public void before(){
+        visitor = new Visitor("Stuart");
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Stuart", visitor.getName());
+    }
 }
