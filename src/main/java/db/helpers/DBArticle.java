72d2ec8ccc1f518676475bc04930f632993c0d76
helenosheaa <helenosheaa@gmail.com>
2018-07-19 15:20:23 +0100

    delete all, get all and find by id for DBArticle

--- src/main/java/db/helpers/DBArticle.java
+++ src/main/java/db/helpers/DBArticle.java
@@ -1,6 +1,21 @@
 package db.helpers;
 
 import db.DBHelper;
+import models.Article;
+
+import java.util.List;
 
 public class DBArticle extends DBHelper {
+
+    public void deleteAll(){
+        deleteAll(Article.class);
+    }
+
+    public List<Article> getAll(){
+        return getAll(Article.class);
+    }
+
+    public Article find(int id){
+        return find(id, Article.class);
+    }
 }
