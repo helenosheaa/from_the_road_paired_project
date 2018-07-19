aa13aec7ee16f3cdf3e41bd4300ebd48898e9e2a
StuartCharters <charters.stuart@gmail.com>
2018-07-19 14:24:50 +0100

    setup writer class

--- src/main/java/models/Writer.java
+++ src/main/java/models/Writer.java
@@ -2,5 +2,42 @@ package models;
 
 import behaviours.IDB;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "writers")
 public class Writer implements IDB {
+
+    private int id;
+    private String name;
+//    private List<Article> articles;
+
+    public Writer(){}
+
+    public Writer(String name){
+        this.name = name;
+    }
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
