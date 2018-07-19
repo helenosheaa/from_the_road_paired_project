b1610391144380253ffbf42080a72ca8cdf5aeab
helenosheaa <helenosheaa@gmail.com>
2018-07-19 15:22:13 +0100

    delete all, get all and find by id for DBTag

--- src/main/java/db/helpers/DBTags.java
+++ src/main/java/db/helpers/DBTags.java
@@ -1,6 +1,22 @@
 package db.helpers;
 
 import db.DBHelper;
+import models.Tag;
+
+import java.util.List;
 
 public class DBTags extends DBHelper {
+
+    public void deleteAll(){
+        deleteAll(Tag.class);
+    }
+
+    public List<Tag> getAll(){
+        return getAll(Tag.class);
+    }
+
+    public Tag find(int id){
+        return find(id, Tag.class);
+    }
+
 }
