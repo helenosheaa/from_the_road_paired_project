779697b1201f242ecb9b20c76261108028bc361d
helenosheaa <helenosheaa@gmail.com>
2018-07-19 14:07:15 +0100

    tag model setup

--- src/main/java/models/Tag.java
+++ src/main/java/models/Tag.java
@@ -2,5 +2,52 @@ package models;
 
 import behaviours.IDB;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "tags")
+
 public class Tag implements IDB {
+
+    private int id;
+    private String name;
+//    private List<Article> articles;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public Tag() {
+    }
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public List<Article> getArticles() {
+//        return articles;
+//    }
+//
+//    public void setArticles(List<Article> articles) {
+//        this.articles = articles;
+//    }
 }
