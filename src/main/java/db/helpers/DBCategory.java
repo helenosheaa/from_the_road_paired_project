6d2afc63079a595ec237e5be2d96c367d795ba50
helenosheaa <helenosheaa@gmail.com>
2018-07-19 15:21:22 +0100

    delete all, get all and find by id for DBCategory

--- src/main/java/db/helpers/DBCategory.java
+++ src/main/java/db/helpers/DBCategory.java
@@ -1,6 +1,22 @@
 package db.helpers;
 
 import db.DBHelper;
+import models.Category;
+
+import java.util.List;
 
 public class DBCategory extends DBHelper {
+
+    public void deleteAll(){
+        deleteAll(Category.class);
+    }
+
+    public List<Category> getAll(){
+        return getAll(Category.class);
+    }
+
+    public Category find(int id){
+        return find(id, Category.class);
+    }
+
 }
