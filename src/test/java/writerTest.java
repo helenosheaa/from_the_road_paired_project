9b734ca823f9501d32f6cf889f5504b729776f35
StuartCharters <charters.stuart@gmail.com>
2018-07-19 15:33:53 +0100

    passing get name for writer

--- src/test/java/writerTest.java
+++ src/test/java/writerTest.java
@@ -1,2 +1,20 @@
+import models.Writer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class writerTest {
+
+    private Writer writer;
+
+    @Before
+    public void before(){
+        writer = new Writer("Helen");
+    }
+
+    @Test
+    public void testGetName(){
+        assertEquals("Helen", writer.getName());
+    }
 }
