acda1d72b8d6e3cbf4973cf95098c983ff3195bb
helenosheaa <helenosheaa@gmail.com>
2018-07-19 16:13:52 +0100

    runner saving

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,9 +1,32 @@
 import db.DBHelper;
+import db.helpers.*;
+import models.*;
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        DBWriter.deleteAll();
+        DBArticle.deleteAll();
+        DBCategory.deleteAll();
+        DBTags.deleteAll();
+        DBVisitor.deleteAll();
 
+        Category category = new Category("America");
+        DBHelper.save(category);
+
+        Tag tag = new Tag("food");
+        DBHelper.save(tag);
+
+        Visitor visitor = new Visitor("Stuart");
+        DBHelper.save(visitor);
+
+        Writer writer = new Writer("Helen");
+        DBHelper.save(writer);
+
+        Article article = new Article("Travelling", writer, "content", "summary" );
+        DBHelper.save(article);
+
+        System.exit(0);
     }
 }
