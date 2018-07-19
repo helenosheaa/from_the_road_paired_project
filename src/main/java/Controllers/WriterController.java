6da7d368c89fc287a7d50e2d04060ca6a62e89f3
helenosheaa <helenosheaa@gmail.com>
2018-07-19 16:44:05 +0100

    index page for writers working

--- src/main/java/Controllers/WriterController.java
+++ src/main/java/Controllers/WriterController.java
@@ -1,5 +1,17 @@
 package Controllers;
 
+import db.DBHelper;
+import db.helpers.DBWriter;
+import models.Writer;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class WriterController {
 
     public WriterController (){
@@ -7,6 +19,17 @@ public class WriterController {
     }
 
     public void setupEndPoints(){
+
+        get("/writers", (req, res) -> {
+            Map<String, Object> model = new HashMap();
+            model.put("template", "templates/writerTemplates/index.vtl");
+
+            List<Writer> writers = DBWriter.getAll();
+            model.put("writers", writers);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
 
     }
 }
