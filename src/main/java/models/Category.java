a1e0c661ad936933ad10832360a0ef06459e68ed
StuartCharters <charters.stuart@gmail.com>
2018-07-19 14:07:12 +0100

    setup Category

--- src/main/java/models/Category.java
+++ src/main/java/models/Category.java
@@ -2,7 +2,49 @@ package models;
 
 import behaviours.IDB;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
 public class Category implements IDB {
 
     private int id;
+    private String name;
+//    private List<Article> articles;
+
+    public Category (){}
+
+    public Category(String name){
+        this.name = name;
+    }
+
+    @Override
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+//    public List<Article> getArticles() {
+//        return articles;
+//    }
+//
+//    public void setArticles(List<Article> articles) {
+//        this.articles = articles;
+//    }
 }
