6a896a7635fdb1ae52993c2246b11de60ca52b48
helenosheaa <helenosheaa@gmail.com>
2018-07-22 15:26:35 +0100

    two nav bars

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -19,6 +19,12 @@ public class AdminController {
 
     public void setupEndPoints() {
 
+        get("/admin", (req, res) -> {
+            Map<String, Object> model= new HashMap();
+            model.put("template", "templates/admin/adminHomeTemplate/index.vtl");
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
 
 
     }
