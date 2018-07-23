69b02c0f03ff194981143a5e87692b8d7cfd83bc
StuartCharters <charters.stuart@gmail.com>
2018-07-23 14:02:33 +0100

    visitor article index is paginated to 3 articles per page

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -33,15 +33,39 @@ public class ArticleController {
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
 //      INDEX
-        get("/articles/:start/:end", (req, res) -> {
+        get("/articles/:page", (req, res) -> {
             Map<String, Object> model = new HashMap();
             model.put("template", "templates/visitor/articleTemplates/index.vtl");
 
             List<Article> articles = DBArticle.getArticlesByDate();
             model.put("articles", articles);
 
-            Map<Integer, List<Integer>> pages = SparkDataHandler.getPagesForList(articles, 3);
+            int numberOnAPage = 3;
+            Map<Integer, Map<String, Integer>> pages = SparkDataHandler.getPagesForList(articles, numberOnAPage);
+
+            int pageNumber = Integer.parseInt(req.params(":page"));
+            Map<String, Integer> page = pages.get(pageNumber);
+            int start = page.get("start");
+            int end = page.get("end");
+
+            boolean isntStartPage = !(pageNumber == 1);
+            boolean isntEndPage = !(pageNumber == pages.size());
+
+            if(isntStartPage){
+                int previousPage = pageNumber - 1;
+                model.put("previousPage", previousPage);
+            }
+
+            if(isntEndPage){
+                int nextPage = pageNumber + 1;
+                model.put("nextPage", nextPage);
+            }
+
+            model.put("isntStartPage", isntStartPage);
+            model.put("isntEndPage", isntEndPage);
             model.put("pages", pages);
+            model.put("start", start);
+            model.put("end", end);
 
             Map<Integer, List<Tag>> articleTags = DBArticle.getMapOfTagsForArticles();
             model.put("articleTags", articleTags);
--- src/main/java/tools/SparkDataHandler.java
+++ src/main/java/tools/SparkDataHandler.java
@@ -1,25 +1,34 @@
 package tools;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public abstract class SparkDataHandler {
 
-    public static Map<Integer, List<Integer>> getPagesForList(List listOfItems, int numberOnAPage){
-        Map<Integer, List<Integer>> pages = new HashMap<>();
+    public static Map<Integer, Map<String, Integer>> getPagesForList(List listOfItems, int numberOnAPage){
+        Map<Integer, Map<String, Integer>> pages = new HashMap<>();
         int numberOfArticles = listOfItems.size();
         int numberOfPages = numberOfArticles / numberOnAPage;
+        int extraArticles = numberOfArticles % numberOnAPage;
         int startIndex = 0;
-        int endIndex = 1;
+        int endIndex = 0;
         for(int page = 1; page <= numberOfPages; page++){
             endIndex += numberOnAPage;
-            List<Integer> startAndEnd = new ArrayList<>();
-            startAndEnd.add(startIndex);
-            startAndEnd.add(endIndex);
+            Map<String, Integer> startAndEnd = new HashMap<>();
+            startAndEnd.put("start", startIndex);
+            startAndEnd.put("end", endIndex);
             pages.put(page, startAndEnd);
             startIndex = endIndex;
+        }
+        if (extraArticles != 0){
+            int page = numberOfPages + 1;
+            startIndex = pages.get(numberOfPages).get("end");
+            endIndex = startIndex + extraArticles;
+            Map<String, Integer> startAndEnd = new HashMap<>();
+            startAndEnd.put("start", startIndex);
+            startAndEnd.put("end", endIndex);
+            pages.put(page, startAndEnd);
         }
         return pages;
     }
