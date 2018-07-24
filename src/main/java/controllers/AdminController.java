9dfa477cb811a95bb8776861a67c0824719159e0
helenosheaa <helenosheaa@gmail.com>
2018-07-24 11:58:11 +0100

    admin index home page

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,9 +1,12 @@
 package controllers;
 
+import db.helpers.DBArticle;
+import models.Article;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
@@ -19,6 +22,9 @@ public class AdminController {
         get("/admin", (req, res) -> {
             Map<String, Object> model= new HashMap();
             model.put("template", "templates/admin/adminHomeTemplate/index.vtl");
+
+            List<Article> articles = DBArticle.getArticlesByDate();
+            model.put("articles", articles);
 
             return new ModelAndView(model, "templates/admin_layout.vtl");
         }, new VelocityTemplateEngine());
